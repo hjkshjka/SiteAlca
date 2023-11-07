0c49e2045b8b360a8a26fb6348662d9a0b0fbc49
Daniel Maia Rocha <118913634+hjkshjka@users.noreply.github.com>
2023-11-07 16:16:46 -0300

    Add files via upload

--- Site-Alcateia-main/site alca/login/login.js
+++ Site-Alcateia-main/site alca/login/login.js
@@ -1,37 +1,18 @@
-let switchCtn = document.querySelector("#switch-cnt");
-let switchC1 = document.querySelector("#switch-c1");
-let switchC2 = document.querySelector("#switch-c2");
-let switchCircle = document.querySelectorAll(".switch__circle");
-let switchBtn = document.querySelectorAll(".switch-btn");
-let aContainer = document.querySelector("#a-container");
-let bContainer = document.querySelector("#b-container");
-let allButtons = document.querySelectorAll(".submit");
+const mode = document.getElementById('mode_icon');
 
-let getButtons = (e) => e.preventDefault()
+mode.addEventListener('click', () => {
+    const form = document.getElementById('login_form');
 
-let changeForm = (e) => {
+    if(mode.classList.contains('fa-moon')) {
+        mode.classList.remove('fa-moon');
+        mode.classList.add('fa-sun');
 
-    switchCtn.classList.add("is-gx");
-    setTimeout(function(){
-        switchCtn.classList.remove("is-gx");
-    }, 1500)
+        form.classList.add('dark');
+        return ;
+    }
+    
+    mode.classList.remove('fa-sun');
+    mode.classList.add('fa-moon');
 
-    switchCtn.classList.toggle("is-txr");
-    switchCircle[0].classList.toggle("is-txr");
-    switchCircle[1].classList.toggle("is-txr");
-
-    switchC1.classList.toggle("is-hidden");
-    switchC2.classList.toggle("is-hidden");
-    aContainer.classList.toggle("is-txl");
-    bContainer.classList.toggle("is-txl");
-    bContainer.classList.toggle("is-z200");
-}
-
-let mainF = (e) => {
-    for (var i = 0; i < allButtons.length; i++)
-        allButtons[i].addEventListener("click", getButtons );
-    for (var i = 0; i < switchBtn.length; i++)
-        switchBtn[i].addEventListener("click", changeForm)
-}
-
-window.addEventListener("load", mainF);
+    form.classList.remove('dark');
+});
